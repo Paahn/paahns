44ad1337efdac1d226cf7e7a047e87743235762c
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-03-29 00:03:55 -0700

    created a sampel test with rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,16 +2,22 @@ require 'rspec'
 require 'capybara/rspec'
 require 'rack/jekyll'
 require 'rack/test'
-require 'pry'RSpec.configure do |config|
+require 'pry'
+
+RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end  config.mock_with :rspec do |mocks|
+  end  
+
+  config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
-  end  # Configure Capybara to use Selenium.
+  end  
+  
+  # Configure Capybara to use Selenium.
   Capybara.register_driver :selenium do |app|
-    # Configure selenium to use Chrome.
-    Capybara::Selenium::Driver.new(app, :browser => :chrome)
-  end  # Configure Capybara to load the website through rack-jekyll.
+  end  
+  
+  # Configure Capybara to load the website through rack-jekyll.
   # (force_build: true) builds the site before the tests are run,
   # so our tests are always running against the latest version
   # of our jekyll site.
