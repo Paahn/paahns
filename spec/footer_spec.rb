f6611d8a4fed1950a9cbfba5e52c6e842b1ff30a
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-04-02 21:24:29 -0700

    testing social links for main page

--- spec/footer_spec.rb
+++ spec/footer_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe "Footer social media links" do
   before :each do
     visit "index.html"
-
   end
+
+    it "has the social media links" do 
+      expect(page).to have_css(".social-me .center-me", count: 4)
+    end
+
 end
